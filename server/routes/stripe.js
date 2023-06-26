0a9e275a1819203eacfa08622b62943958364b69
dev-lawrence <lawrenceamrasakpare19@gmail.com>
2023-06-26 20:39:49 +0100

    completed the stripe integration

--- server/routes/stripe.js
+++ server/routes/stripe.js
@@ -26,6 +26,56 @@ router.post('/create-checkout-session', async (req, res) => {
   );
 
   const session = await stripe.checkout.sessions.create({
+    shipping_address_collection: {
+      allowed_countries: ['US', 'CA', 'NG'],
+    },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 0,
+            currency: 'usd',
+          },
+          display_name: 'Free shipping',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 5,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 7,
+            },
+          },
+        },
+      },
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: 1500,
+            currency: 'usd',
+          },
+          display_name: 'Next day air',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 1,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 1,
+            },
+          },
+        },
+      },
+    ],
+
+    phone_number_collection: {
+      enabled: true,
+    },
+
     line_items: formattedLineItems,
     mode: 'payment',
     success_url: `${process.env.CLIENT_URL}/checkout-success`,
